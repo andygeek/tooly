d2a2e33f8faded5ffabea827c6a2babc7c44d4db
andygeek <andy.eulogio@gmail.com>
2022-11-04 22:51:37 -0500

    1ff2ea7

---
+++ _nuxt/static/1667620158/documentation/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ftooly\u002F_nuxt\u002Fstatic\u002F1667620158",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdocumentation",config:{_app:{basePath:"\u002Ftooly\u002F",assetsPath:"\u002Ftooly\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"6fcf98ae"}}}}(null));
