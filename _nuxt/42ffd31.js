7065226906a71fbb3c4ad79933cdb27eafe7c329
andygeek <andy.eulogio@gmail.com>
2025-01-26 12:47:24 -0500

    3cdc7a5

---
+++ _nuxt/42ffd31.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{498:function(e,n,t){"use strict";t.r(n);var l=t(0).default.extend({name:"IndexPage"}),o=t(62),component=Object(o.a)(l,(function(){var e=this._self._c;this._self._setupProxy;return e("Tooly")}),[],!1,null,null,null);n.default=component.exports;installComponents(component,{Tooly:t(495).default})}}]);
---
+++ _nuxt/static/1737913638/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ftooly\u002F_nuxt\u002Fstatic\u002F1737913638",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Ftooly\u002F",assetsPath:"\u002Ftooly\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"491a7432"}}}}(null));
